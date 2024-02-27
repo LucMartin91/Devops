38f2bb162b3242cd53293b63906f44a5df993e64
LucMartin91 <105196759+LucMartin91@users.noreply.github.com>
2024-02-27 12:49:36 +0100

    Update DemoApplicationTests.java pour les tests jUnits

--- demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,27 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void testAdd() {
+        assertEquals(5, Calculator.add(2, 3));
+    }
+
+    @Test
+    void testMulti() {
+        assertEquals(6, Calculator.multi(2, 3));
+    }
+
+    @Test
+    void testDiv() {
+        assertEquals(2, Calculator.div(6, 3));
+    }
 }
